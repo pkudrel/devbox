a8c846477a4999eea1815c7e1ae6a6d2b7f802fc
Piotr Kudrel <piotr.kudrel@deneblab.com>
2019-01-04 14:31:46 +0100

    Fix pure

--- linux/install-pure-zsh.sh
+++ linux/install-pure-zsh.sh
@@ -4,6 +4,7 @@ mkdir -p ~/.local/misc/pure
 wget -O ~/.local/misc/pure/pure.zsh https://raw.githubusercontent.com/sindresorhus/pure/master/pure.zsh
 wget -O ~/.local/misc/pure/async.zsh https://raw.githubusercontent.com/sindresorhus/pure/master/async.zsh
 
-mkdir -p ~/.oh-my-zsh/functions
-ln -s ~/.local/misc/pure/pure.zsh ~/.oh-my-zsh/functions/pure.zsh
-ln -s ~/.local/misc/pure/async.zsh ~/.oh-my-zsh/functions/async.zsh
+mkdir -p ~/zfunctions
+ln -s ~/.local/misc/pure/pure.zsh ~/zfunctions/prompt_pure_setup
+ln -s ~/.local/misc/pure/async.zsh ~/zfunctions/async
+
