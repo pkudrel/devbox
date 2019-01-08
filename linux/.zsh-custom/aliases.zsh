5f3010ba0249acf0a65b2e6664ddd35c6786de0c
Piotr Kudrel <piotr.kudrel@deneblab.com>
2019-01-08 14:48:12 +0100

    Work on linux files

--- linux/.zsh-custom/aliases.zsh
+++ linux/.zsh-custom/aliases.zsh
@@ -1,6 +1,6 @@
 # Aliases
 alias d="docker"
-alias code="/c/mnt/Users/piotr/AppData/Local/Programs/Microsoft\ VS\ Code/Code.exe"
+alias code="/mnt/c/Users/piotr/AppData/Local/Programs/Microsoft\ VS\ Code/Code.exe"
 alias devbox-update="sudo ansible-playbook ~/.devbox/linux/devenv-playbook.yml"
 alias devbox-update-dev="sudo ansible-playbook -e devbox_dev=true ~/.devbox/linux/devenv-playbook.yml"
 alias devrc="vi ~/.devbox/linux/devenv-playbook.yml"
