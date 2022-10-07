0e29d0827bf3a9067e3db3737eed9750ee4bdf93
Piotr Kudrel <piotr.kudrel@deneblab.com>
2022-10-07 11:20:49 +0800

    SAVEPOINT

--- windows/powershell/profile.ps1
+++ windows/powershell/profile.ps1
@@ -16,8 +16,8 @@ $env:POSH_GIT_ENABLED = $true
 Import-Module-With-Measure oh-my-posh
 Import-Module-With-Measure Terminal-Icons
 Import-Module-With-Measure DockerCompletion
-if ($host.Name -eq 'ConsoleHost')
-{
+if ($host.Name -eq 'ConsoleHost') {
+    # https://github.com/PowerShell/PSReadLine#install-from-powershellgallery-preferred
     Import-Module-With-Measure PSReadLine
     Set-PSReadLineKeyHandler -Key Tab -Function Complete
     Set-PSReadLineOption -HistoryNoDuplicates -EditMode Windows
@@ -31,3 +31,22 @@ if ($host.Name -eq 'ConsoleHost')
 #Set-Theme Paradox
 #Set-PoshPrompt -Theme Paradox
 Set-PoshPrompt -Theme W:\DenebLab\devbox\windows\windows-terminal\paradox.omp.json
+
+## Aliases
+
+function OpenPSProfileFile { code $Home\Documents\PowerShell\Profile.ps1 }
+function DockerKillContainerFn ([string]$ContainerName) {
+    #Write-Host $ContainerName
+    $output = docker.exe ps --filter name=$ContainerName --quiet | Format-List -Property "CONTAINER ID" | out-string
+    $conteinerId = $output.Trim()
+    if ([string]::IsNullOrEmpty($conteinerId)) {
+        Write-Host "Cannot find container with name '$ContainerName'"
+    }
+    else {
+        docker kill $conteinerId
+    }
+   
+}
+
+Set-Alias psprof OpenPSProfileFile -Option ReadOnly
+Set-Alias dkKillContainer DockerKillContainerFn -Option ReadOnly
