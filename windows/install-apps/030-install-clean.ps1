4f99909ab176271af9dc989dbc04b05f8627aff1
Piotr Kudrel <piotr.kudrel@deneblab.com>
2023-05-20 10:25:02 +0800

    SAVEPOINT

---
+++ windows/install-apps/030-install-clean.ps1
@@ -0,0 +1,5 @@
+### run this 'sudo .\windows\install-apps\030-install-clean.ps1'
+
+## OneDrive
+Import-Module -DisableNameChecking $PSScriptRoot\030-install-clean-data\030-install-clean-onedrive.ps1
+RemoveOneDrive
